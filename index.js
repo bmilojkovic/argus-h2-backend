e4c15c6fbe794ee8042051ddce7e3b1afd24d829
bmilojkovic <bandza88@gmail.com>
2025-09-17 12:11:12 +0200

    Added support for weapons, familiars and elemental data.

--- index.js
+++ index.js
@@ -49,19 +49,21 @@ function broadcastInfo(postData, jwtToken) {
     });
 }
 
-const primaryBoonGods = ["Aphrodite", "Apollo", "Ares", "Demeter", "Haephestus", "Hera", "Hestia", "Poseidon", "Zeus"]
-const primaryBoonTypes = ["Weapon", "Special", "Cast", "Sprint", "Mana"]
-const boonRarities = ["Common", "Rare", "Epic", "Heroic", "Legendary", "Duo", "Infusion"]
+const dataSeparator = ";;";
+const primaryBoonGods = ["Aphrodite", "Apollo", "Ares", "Demeter", "Haephestus", "Hera", "Hestia", "Poseidon", "Zeus"];
+const primaryBoonTypes = ["Weapon", "Special", "Cast", "Sprint", "Mana"];
+const boonRarities = ["Common", "Rare", "Epic", "Heroic", "Legendary", "Duo", "Infusion"];
+const weaponRarities = ["Common", "Rare", "Epic", "Heroic", "Legendary"];
 
-function parseBoonList(boonList) {
-  runData = {
+function parseBoonData(boonData) {
+  var parsedData = {
     "otherBoons" : []
   };
-  boonList.forEach( (boon) => {
-    splitBoon = boon.split("-");
-    if (splitBoon.length == 1) { //no dash, so we assume we got only the name
-      boonName = boon;
-      boonRarity = "Common";
+  boonData.forEach( (boon) => {
+    splitBoon = boon.split(dataSeparator);
+    if (splitBoon.length != 2) { //data should contain boon rarity and name
+      console.log("Invalid boon data. Couldn't parse.");
+      return {};
     } else {
       boonRarity = splitBoon[0];
       if (!(boonRarities.includes(boonRarity))) {
@@ -89,20 +91,102 @@ function parseBoonList(boonList) {
     }
 
     if (foundGod != null && foundType != null) {
-      runData[foundType.toLowerCase() + "Boon"] = {}
-      runData[foundType.toLowerCase() + "Boon"].name = foundGod + foundType + "Boon";
-      runData[foundType.toLowerCase() + "Boon"].rarity = boonRarity;
+      parsedData[foundType.toLowerCase() + "Boon"] = {}
+      parsedData[foundType.toLowerCase() + "Boon"].name = foundGod + foundType + "Boon";
+      parsedData[foundType.toLowerCase() + "Boon"].rarity = boonRarity;
     } else {
-      runData["otherBoons"].push({name: boonName, rarity: boonRarity})
+      parsedData["otherBoons"].push({name: boonName, rarity: boonRarity})
     }
   });
 
-  return runData;
+  return parsedData;
+}
+
+const emptyWeaponString = "NOWEAPON";
+function parseWeaponData(weaponData) {
+  if (weaponData === emptyWeaponString) {
+    return {};
+  }
+
+  var parsedData = {};
+
+  splitWeapon = weaponData.split(dataSeparator);
+  if (splitWeapon.length != 2) { //data should contain rarity and name
+    console.log("Invalid weapon data. Couldn't parse.");
+    return {};
+  } else {
+    weaponRarity = splitWeapon[0];
+    if (!(weaponRarities.includes(weaponRarity))) {
+      weaponRarity = "Common";
+    }
+
+    weaponName = splitWeapon[1];
+
+    weaponDescription = splitWeapon[2];
+  }
+
+  parsedData["name"] = weaponName;
+  parsedData["rarity"] = weaponRarity;
+  parsedData["description"] = weaponDescription;
+  return parsedData;
+}
+
+const emptyFamiliarString = "NOFAMILIAR";
+function parseFamiliarData(familiarData) {
+  if (familiarData === emptyFamiliarString) {
+    return {};
+  }
+
+  var parsedData = {}
+
+  parsedData["name"] = familiarData;
+
+  return parsedData;
+}
+
+/*
+* Elements should arrive in the following format:
+* [elementA]:[numberA];;[elementB]:[numberB];;etc
+*
+* For example:
+* Fire:1;;Water:0;;Earth:3;;Air:0;;Aether:0
+*/
+const emptyElementsString = "NOELEMENTS";
+function parseElementalData(elementalData) {
+  if (elementalData === emptyElementsString) {
+    return {};
+  }
+
+  var parsedData = [];
+  var unorderedData = {};
+  var elementsArray = elementalData.split(dataSeparator); //get all the elements
+  elementsArray.forEach((element) => {
+    var splitElement = element.split(":"); //0 is element name, 1 is value
+    unorderedData[splitElement[0]] = splitElement[1];
+  });
+
+  var orderedElementNames = ["Earth", "Water", "Air", "Fire", "Aether"]
+  orderedElementNames.forEach(element => {
+    parsedData.push({name: element, value: unorderedData[element]});  
+  })
+
+  return parsedData;
 }
 
 app.post("/run_info", (req, res, next) => {
+  console.log("Received data: " + JSON.stringify(req.body));
+  
   const broadcasterId = req.body.user;
-  const runData = parseBoonList(req.body.boonList.split(" "))
+  const boonData = parseBoonData(req.body.boonData.split(" "));
+  const weaponData = parseWeaponData(req.body.weaponData);
+  const familiarData = parseFamiliarData(req.body.familiarData);
+  const elementalData = parseElementalData(req.body.elementalData);
+  const runData = {
+    boonData: boonData,
+    weaponData: weaponData,
+    familiarData: familiarData,
+    elementalData: elementalData
+  };
   jwtToken = buildToken(broadcasterId);
   broadcastMessage = {
     message: JSON.stringify(runData),
