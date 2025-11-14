dea211c41813e471b062f7329d59a4702c3b18c0
bmilojkovic <bandza88@gmail.com>
2025-11-14 10:09:19 +0100

    Fixing variable declaration error

--- app_updater.mjs
+++ app_updater.mjs
@@ -1,7 +1,7 @@
 import app_versions from "./app_versions.json" with { type: 'json' };
 
 export function handleGetNewestAppVersion(req) {
-  response_object = {};
+  let response_object = {};
   response_object["newest_version"] = app_versions.newest_version;
   response_object["details"] =
     app_versions.versions[app_versions.newest_version];
