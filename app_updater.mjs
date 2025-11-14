eccf52704e029b5f01264c4a520318808fc11310
bmilojkovic <bandza88@gmail.com>
2025-11-14 18:16:34 +0100

    Changing update logic to include changelog as well

--- app_updater.mjs
+++ app_updater.mjs
@@ -1,8 +1,10 @@
-const newest_app_version = "1.1.0";
+import app_versions from "./app_versions.json" with { type: 'json' };
 
 export function handleGetNewestAppVersion(req, res) {
   let response_object = {};
-  response_object["newest_version"] = newest_app_version;
+  response_object["newest_version"] = app_versions.newest_version;
+  response_object["changelog"] =
+    app_versions.changelogs[app_versions.newest_version];
 
   res.send(JSON.stringify(response_object));
 }
