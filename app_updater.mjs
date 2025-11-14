45b9e1d38c890400f8c396804895fa2682ecbf82
bmilojkovic <bandza88@gmail.com>
2025-11-14 10:11:01 +0100

    Fixing GET response error

--- app_updater.mjs
+++ app_updater.mjs
@@ -1,10 +1,10 @@
 import app_versions from "./app_versions.json" with { type: 'json' };
 
-export function handleGetNewestAppVersion(req) {
+export function handleGetNewestAppVersion(req, res) {
   let response_object = {};
   response_object["newest_version"] = app_versions.newest_version;
   response_object["details"] =
     app_versions.versions[app_versions.newest_version];
 
-  req.send(JSON.stringify(response_object));
+  res.send(JSON.stringify(response_object));
 }
