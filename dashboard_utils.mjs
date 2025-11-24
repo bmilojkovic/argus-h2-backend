3596baf6a86e070cb99b634d75db8eda9f13579a
bmilojkovic <bandza88@gmail.com>
2025-11-24 10:42:48 +0100

    Refactoring the dashboard cleaning procedure

--- dashboard_utils.mjs
+++ dashboard_utils.mjs
@@ -1,11 +1,26 @@
 import cron from "node-cron";
 import { readStorageObject, writeStorageObject } from "./aws_storage.mjs";
 
+function cleanSurplus(dashboardRunData) {
+  //remove any entries older than 10min
+  dashboardRunData = Object.keys(dashboardRunData).reduce(
+    (accumulator, key) => {
+      if (dashboardRunData[key].timestamp > currentTimestamp - 10 * 60 * 1000) {
+        accumulator[key] = dashboardRunData[key];
+        return accumulator;
+      }
+    },
+    {}
+  );
+
+  return dashboardRunData;
+}
+
 /**
  * Updates the object in S3 storage
  */
-export function updateDasboardRunData(newRunData, twitchProfile) {
-  const dashboardRunData = readStorageObject("dashboardRunData");
+export async function updateDasboardRunData(newRunData, twitchProfile) {
+  const dashboardRunData = await readStorageObject("dashboardRunData");
 
   //if it is not there, we just exit
   if (dashboardRunData == null && newRunData == null) {
@@ -23,20 +38,17 @@ export function updateDasboardRunData(newRunData, twitchProfile) {
     dashboardRunData[twitchProfile.twitchId] = objectToStore;
   }
 
-  //remove any entries older than 10min
-  dashboardRunData = Object.keys(dashboardRunData).reduce(
-    (accumulator, key) => {
-      if (dashboardRunData[key].timestamp > currentTimestamp - 10 * 60 * 1000) {
-        accumulator[key] = dashboardRunData[key];
-        return accumulator;
-      }
-    },
-    {}
-  );
-
-  writeStorageObject("dashboardRunData", dashboardRunData);
+  await writeStorageObject("dashboardRunData", dashboardRunData);
 }
 
-cron.schedule("* * * * *", () => {
-  updateDasboardRunData(null, null);
-});
+async function timedClean() {
+  const dashboardRunData = await readStorageObject("dashboardRunData");
+  //if it is not there, we just exit
+  if (dashboardRunData == null && newRunData == null) {
+    return;
+  }
+  dashboardRunData = cleanSurplus(dashboardRunData);
+  await writeStorageObject("dashboardRunData", dashboardRunData);
+}
+
+cron.schedule("* * * * *", timedClean);
